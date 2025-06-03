57abe16190fe8789d0036e1eba085d707ba6cb26
mujahid-ansari-16 <mmujahid162004@gmail.com>
2025-06-03 18:47:58 +0530

    update url

--- app.js
+++ app.js
@@ -95,7 +95,13 @@ app.use(errorsController.pageNotFound);
 
 const PORT = 3000;
 
-mongoose.connect(DB_PATH).then(() => {
+const DB_CONNECTION_STRING =  process.env.DB_PATH;
+if (!DB_CONNECTION_STRING) {
+  console.error('DB_PATH is not set in the environment variables.');
+  process.exit(1);
+}
+
+mongoose.connect(DB_CONNECTION_STRING ).then(() => {
   console.log('Connected to Mongo');
   app.listen(PORT, () => {
     console.log(`Server running on address http://localhost:${PORT}`);
